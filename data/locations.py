d2e49173f5714834fdeed9e85097c5340174bc13
Vaganuki <hugo.warnotte@gmail.com>
2026-08-31 14:48:17 +0200

    (update): added a few locations

--- data/locations.py
+++ data/locations.py
@@ -1,3 +1,15 @@
+
+# === WORKSHOPS ===
 BANK = {'x': 4, 'y': 1}
-COPPER_ROCKS = {'x': 2, 'y': 0}
 MINING_WORKSHOP = {'x': 1, 'y': 5}
+
+
+# === MINES ===
+
+COPPER_ROCKS = {'x': 2, 'y': 0}
+
+# === MONSTERS ===
+
+CHICKEN =  {'x': 0, 'y': 1}
+YELLOW_SLIME =  {'x': 1, 'y': -2}
+GREEN_SLIME =  {'x': 0, 'y': -1}
