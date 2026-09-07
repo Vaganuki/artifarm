2fc80b38aa866cbbdd751bde809a787efa3bd3ac
Vaganuki <hugo.warnotte@gmail.com>
2026-09-07 16:24:52 +0200

    (feat): this is for tomorrow

---
+++ src/components/artifact-logs/ArtifactLogs.tsx
@@ -0,0 +1,12 @@
+
+interface ArtifactLogs {
+    character: string;
+    message: string;
+    type: 'farm' | 'fight' | 'cooldown' | 'error';
+}
+
+export function artifactLogs() {
+    return(
+        <div></div>
+    )
+}
